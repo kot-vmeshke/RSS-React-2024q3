b6ebeaa8ac843b5fd865bc01127f87fffb2c09a8
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-07 21:14:47 +0200

    feat: add loader

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -2,13 +2,44 @@ import { Component } from 'react';
 
 import './BooksList.scss';
 import { BookCard } from '../BookCard/BookCard';
+import { Loader } from '../Loader/Loader';
 
-class BooksList extends Component {
+interface BooksListProps {}
+interface BooksListState {
+  booksList: string[];
+  isLoaded: boolean;
+}
+
+class BooksList extends Component<BooksListProps, BooksListState> {
+  constructor(props: BooksListProps) {
+    super(props);
+    this.state = {
+      booksList: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount(): void {
+    setTimeout(() => {
+      this.setState({ isLoaded: true });
+    }, 3000);
+  }
   render() {
+    const { booksList, isLoaded } = this.state;
     return (
-      <ul className="books-list">
-        <BookCard />
-      </ul>
+      <>
+        {isLoaded ? (
+          <ul className="books-list">
+            {booksList.length ? (
+              <BookCard />
+            ) : (
+              <span className="books-list__nothing">Nothing was found</span>
+            )}
+          </ul>
+        ) : (
+          <Loader />
+        )}
+      </>
     );
   }
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import { Main } from './Main/Main';
 import { BooksList } from './BooksList/BooksList';
 import { BookCard } from './BookCard/BookCard';
 import { ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
+import { Loader } from './Loader/Loader';
 
-export { Header, SearchBar, Main, BooksList, BookCard, ErrorBoundary };
+export { Header, SearchBar, Main, BooksList, BookCard, ErrorBoundary, Loader };
