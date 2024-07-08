0fdefdd7dcf398a7f1397b30d2e5c0e0077f18e4
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-08 20:35:06 +0200

    refactor: loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import './Loader.scss';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="loader-wrapper">
-        <div className="loader"></div>
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader"></div>
+    </div>
+  );
+};
 
 export { Loader };
