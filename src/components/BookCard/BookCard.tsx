edd1c91d24b7c3eeead8834a362586bc64688558
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-08 19:41:14 +0200

    refactor: book-card component

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
+import { FC } from 'react';
 
 import './BookCard.scss';
 import { Book } from '../BooksList/BooksList';
 
-class BookCard extends Component<Book> {
-  render() {
-    const { authors, title, subjects } = this.props;
-    return (
-      <li className="book">
-        <span className="book__author">
-          {authors.map((author) => author.name).join(', ')}
-        </span>
-        <span className="book__name">{title}</span>
-        <span className="book__subjects">Subjects: {subjects.join(', ')}.</span>
-      </li>
-    );
-  }
-}
+const BookCard: FC<Book> = ({ authors, title, subjects }) => {
+  return (
+    <li className="book">
+      <span className="book__author">
+        {authors.map((author) => author.name).join(', ')}
+      </span>
+      <span className="book__name">{title}</span>
+      <span className="book__subjects">Subjects: {subjects.join(', ')}.</span>
+    </li>
+  );
+};
 
 export { BookCard };
