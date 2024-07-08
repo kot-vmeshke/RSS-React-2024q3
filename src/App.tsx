5fd330c3cc81c4b2f3148f2a10d440c49009dd05
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-08 19:37:47 +0200

    refactor: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { Component } from 'react';
 import './App.scss';
 import { SearchPage } from './pages';
 import { ErrorBoundary } from './components';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <SearchPage />
-      </ErrorBoundary>
-    );
-  }
-}
+const App = () => {
+  return (
+    <ErrorBoundary>
+      <SearchPage />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
