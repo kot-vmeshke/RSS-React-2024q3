5b570e22b93366a49be0f3ed59944f2d63984a30
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-08 21:40:37 +0200

    refactor: search-bar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,44 +1,38 @@
-import { Component } from 'react';
+import { FC, useState } from 'react';
 import SearchIcon from '../../assets/search-sm.svg';
 import './SearchBar.scss';
-import { SearchBarProps, SearchBarState } from '../../types';
+import { SearchBarProps } from '../../types';
 
-class SearchBar extends Component<SearchBarProps, SearchBarState> {
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.state = {
-      query: localStorage.getItem('books-search') || '',
-    };
-  }
+const SearchBar: FC<SearchBarProps> = ({ updateSearchString }) => {
+  const [query, setQuery] = useState<string>(
+    localStorage.getItem('books-search') || ''
+  );
 
-  render() {
-    const { query } = this.state;
-    return (
-      <div className="search">
-        <input
-          className="search__input"
-          type="text"
-          name="search"
-          id="search"
-          placeholder="author name"
-          value={query}
-          onChange={(e) => {
-            this.setState({ query: e.target.value });
-          }}
-          onKeyUp={(e) => {
-            if (e.key === 'Enter') this.props.updateSearchString(query);
-          }}
-        />
-        <button
-          className="search__button"
-          type="button"
-          onClick={() => this.props.updateSearchString(query)}
-        >
-          <img src={SearchIcon} alt="" width={20} />
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search">
+      <input
+        className="search__input"
+        type="text"
+        name="search"
+        id="search"
+        placeholder="author name"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          if (e.key === 'Enter') updateSearchString(query);
+        }}
+      />
+      <button
+        className="search__button"
+        type="button"
+        onClick={() => updateSearchString(query)}
+      >
+        <img src={SearchIcon} alt="" width={20} />
+      </button>
+    </div>
+  );
+};
 
 export { SearchBar };
