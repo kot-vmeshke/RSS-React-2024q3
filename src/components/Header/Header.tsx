a423a1bda2916991d27cec5e64f1263a59d8296c
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-08 20:32:49 +0200

    refactor: header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,21 @@
-import { Component } from 'react';
+import { FC } from 'react';
 
 import './Header.scss';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchBarProps } from '../../types';
 
-class Header extends Component<SearchBarProps> {
-  constructor(props: SearchBarProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <header className="header">
-        <div className="container header__container">
-          <h1 className="header__title">Search books</h1>
-          <SearchBar
-            searchString={this.props.searchString}
-            updateSearchString={this.props.updateSearchString}
-          />
-        </div>
-      </header>
-    );
-  }
-}
+const Header: FC<SearchBarProps> = ({ searchString, updateSearchString }) => {
+  return (
+    <header className="header">
+      <div className="container header__container">
+        <h1 className="header__title">Search books</h1>
+        <SearchBar
+          searchString={searchString}
+          updateSearchString={updateSearchString}
+        />
+      </div>
+    </header>
+  );
+};
 
 export { Header };
