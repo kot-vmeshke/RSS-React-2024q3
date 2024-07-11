ff4ea1e4a551c591ae440030183870db8d2c9aa0
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-11 23:36:43 +0200

    refactor: main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,42 +1,16 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import './Main.scss';
 import { BooksList } from '../BooksList/BooksList';
-import { MainProps, MainState } from '../../types';
+import { MainProps } from '../../types';
 
-class Main extends Component<MainProps, MainState> {
-  constructor(props: MainProps) {
-    super(props);
-    this.state = {
-      error: false,
-    };
-  }
-
-  componentDidUpdate(): void {
-    if (this.state.error) {
-      throw new Error('You clicked the button to create a test bug');
-    }
-  }
-
-  render() {
-    return (
-      <main className="main">
-        <div className="container main__container">
-          <button
-            className="main-button"
-            onClick={() => {
-              this.setState({ error: true });
-            }}
-          >
-            Create Error
-          </button>
-          <BooksList
-            isLoaded={this.props.isLoaded}
-            booksList={this.props.booksList}
-          />
-        </div>
-      </main>
-    );
-  }
-}
+const Main: FC<MainProps> = ({ isLoaded, booksList }) => {
+  return (
+    <main className="main">
+      <div className="container main__container" data-testid="main-container">
+        <BooksList isLoaded={isLoaded} booksList={booksList} />
+      </div>
+    </main>
+  );
+};
 
 export { Main };
