a55400989d0a167aa68e652ece2bb0637d5f75c2
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-07-14 15:19:05 +0200

    feat: add another tests for search-bar

--- tests/SearchBar.test.tsx
+++ tests/SearchBar.test.tsx
@@ -1,3 +1,29 @@
-import { test } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { SearchBar } from '../src/components';
+import { describe, expect, it } from 'vitest';
 
-test.todo('Tests for details');
+describe('SearchBar', () => {
+  it('Clicking the Search button saves the entered value to the local storage', () => {
+    render(<SearchBar updateSearchString={() => {}} />);
+
+    const input = screen.getByRole('textbox');
+    const testValue = 'test value';
+    fireEvent.change(input, { target: { value: testValue } });
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(localStorage.getItem('books-search')).toBe(testValue);
+  });
+
+  it('Component retrieves the value from the local storage upon mounting', () => {
+    const testValue = 'mount test value';
+    localStorage.setItem('books-search', testValue);
+
+    render(<SearchBar updateSearchString={() => {}} />);
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+
+    expect(input.value).toBe(testValue);
+  });
+});
